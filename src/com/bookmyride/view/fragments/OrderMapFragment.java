aad53dc0023d4cbe5c702e98a88499e6f07db59d
sanjaysaini1 <sanjaysaini1@gmail.com>
2014-11-30 15:36:52 +0530

    Orders related changes todo creare order cars fragment

--- src/com/bookmyride/view/fragments/OrderMapFragment.java
+++ src/com/bookmyride/view/fragments/OrderMapFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment{
+public class OrderMapFragment extends Fragment{
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
